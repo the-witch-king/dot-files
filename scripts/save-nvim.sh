dbd1bddad204f284b358bf5406f34cea02336109
Michael Churilov <michael.churilov@gmail.com>
2020-03-09 09:28:36 -0400

    Add vim copy script, update coc-settings

---
+++ scripts/save-nvim.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Saving VIM settings."
+
+cp ~/.config/nvim/init.vim ../.config/nvim/init.vim
+cp ~/.config/nvim/coc-settings.json ../.config/nvim/coc-settings.json
+
+echo "Save complete."
