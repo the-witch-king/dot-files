4ae7ff4df6afc24870ef03c77b5a1cfa4812af5a
Michael Churilov <michael.churilov@gmail.com>
2024-02-22 20:09:36 -0500

    Update dot files

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,1 +1,2 @@
-return { { "sainnhe/everforest" }, { "LazyVim/LazyVim", opts = { colorscheme = "everforest" } } }
+-- return { { "sainnhe/everforest" }, { "LazyVim/LazyVim", opts = { colorscheme = "everforest" } } }
+return { { "morhetz/gruvbox" }, { "LazyVim/LazyVim", opts = { colorscheme = "gruvbox" } } }
